124b3533db87737bad1b4a45111c3223a802cf89
theubie <randell@nvcreativetechnology.com>
2023-04-27 13:21:57 -0500

    Fixed Oooba changing generation AGAIN.

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import json
 import modules.shared as shared
 import modules.chat as chat
 import pickle
+from modules.chat import replace_all
 from modules.extensions import apply_extensions
 from modules.text_generation import encode, get_max_prompt_length
 
@@ -17,7 +18,7 @@ memory_settings = {"position": "Before Context"}
 memory_select = None
 
 
-def custom_generate_chat_prompt(user_input, state, end_of_turn="", **kwargs):
+def custom_generate_chat_prompt(user_input, state, **kwargs):
     impersonate = kwargs['impersonate'] if 'impersonate' in kwargs else False
     _continue = kwargs['_continue'] if '_continue' in kwargs else False
     also_return_rows = kwargs['also_return_rows'] if 'also_return_rows' in kwargs else False
@@ -58,38 +59,51 @@ def custom_generate_chat_prompt(user_input, state, end_of_turn="", **kwargs):
         chat_prompt_size -= shared.soft_prompt_tensor.shape[1]
 
     max_length = min(get_max_prompt_length(state), chat_prompt_size)
-    if is_instruct:
-        prefix1 = f"{state['name1']}\n"
-        prefix2 = f"{state['name2']}\n"
-    else:
-        prefix1 = f"{state['name1']}: "
-        prefix2 = f"{state['name2']}: "
 
+    # Building the turn templates
+    if 'turn_template' not in state or state['turn_template'] == '':
+        if is_instruct:
+            template = '<|user|>\n<|user-message|>\n<|bot|>\n<|bot-message|>\n'
+        else:
+            template = '<|user|>: <|user-message|>\n<|bot|>: <|bot-message|>\n'
+    else:
+        template = state['turn_template'].replace(r'\n', '\n')
+
+    replacements = {
+        '<|user|>': state['name1'].strip(),
+        '<|bot|>': state['name2'].strip(),
+    }
+
+    user_turn = replace_all(template.split('<|bot|>')[0], replacements)
+    bot_turn = replace_all('<|bot|>' + template.split('<|bot|>')[1], replacements)
+    user_turn_stripped = replace_all(user_turn.split('<|user-message|>')[0], replacements)
+    bot_turn_stripped = replace_all(bot_turn.split('<|bot-message|>')[0], replacements)
+
+    # Building the prompt
     i = len(shared.history['internal']) - 1
     while i >= 0 and len(encode(''.join(rows))[0]) < max_length:
         if _continue and i == len(shared.history['internal']) - 1:
-            rows.insert(1, f"{prefix2}{shared.history['internal'][i][1]}")
+            rows.insert(1, bot_turn_stripped + shared.history['internal'][i][1].strip())
         else:
-            rows.insert(1, f"{prefix2}{shared.history['internal'][i][1].strip()}{state['end_of_turn']}\n")
+            rows.insert(1, bot_turn.replace('<|bot-message|>', shared.history['internal'][i][1].strip()))
 
         string = shared.history['internal'][i][0]
         if string not in ['', '<|BEGIN-VISIBLE-CHAT|>']:
-            this_prefix1 = prefix1.replace('<|round|>', f'{i}')  # for ChatGLM
-            rows.insert(1, f"{this_prefix1}{string.strip()}{state['end_of_turn']}\n")
+            rows.insert(1, replace_all(user_turn, {'<|user-message|>': string.strip(), '<|round|>': str(i)}))
 
         i -= 1
 
     if impersonate:
         min_rows = 2
-        rows.append(f"{prefix1.strip() if not is_instruct else prefix1}")
+        rows.append(user_turn_stripped.rstrip(' '))
     elif not _continue:
         # Adding the user message
         if len(user_input) > 0:
-            this_prefix1 = prefix1.replace('<|round|>', f'{len(shared.history["internal"])}')  # for ChatGLM
-            rows.append(f"{this_prefix1}{user_input}{state['end_of_turn']}\n")
+            rows.append(replace_all(user_turn, {'<|user-message|>': user_input.strip(),
+                                                '<|round|>': str(len(shared.history["internal"]))}))
 
         # Adding the Character prefix
-        rows.append(apply_extensions("bot_prefix", f"{prefix2.strip() if not is_instruct else prefix2}"))
+        rows.append(apply_extensions("bot_prefix", bot_turn_stripped.rstrip(' ')))
 
     while len(rows) > min_rows and len(encode(''.join(rows))[0]) >= max_length:
         rows.pop(1)
@@ -306,7 +320,8 @@ def ui():
 
     shared.gradio['character_menu'].change(load_character_complex_memory_hijack,
                                            [shared.gradio[k] for k in ['character_menu', 'name1', 'name2', 'mode']],
-                                           [shared.gradio[k] for k in ['name1', 'name2', 'character_picture', 'greeting', 'context', 'end_of_turn', 'display']]).then(pairs_loaded, None, memory_select)
+                                           [shared.gradio[k] for k in ['name1', 'name2', 'character_picture', 'greeting', 'context', 'turn_template', 'display']])
+
     # Return the UI elements wrapped in a Gradio column
     # return c
 
